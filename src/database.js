651767743effd52cd81c9e363872f6c826b68945
Santhanam-san <ranaranran@gmail.com>
2024-03-30 19:20:23 +0530

    .env file added and connected to render's psql db

--- src/database.js
+++ src/database.js
@@ -1,14 +1,24 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'postgres',  
-  host: 'localhost',    // Database host
-  port: 5432,           // Database port
-  username:"santhanam", // Database username
-  password: "12345678", // Database password
-  database: "FoodDeliverybackend-Santhanam", // Database name
+// const sequelize = new Sequelize({
+//   dialect: 'postgres',  
+//   host: 'localhost',    // Database host
+//   port: 5432,           // Database port
+//   database: "FoodDeliverybackend-Santhanam", // Database name
+//   username:"santhanam", // Database username
+//   password: "12345678", // Database password
+ 
+// });
+const dbConnection = process.env.DB_CONNECTION;
+const sequelize = new Sequelize(dbConnection, {
+  dialectOptions: {
+    ssl: {
+      require: true, // Require SSL/TLS
+      rejectUnauthorized: false // Accept self-signed certificates (if applicable)
+    }
+  }
 });
-
 
 async function testDatabaseConnection() {
   try {
